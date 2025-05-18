39d84fd8d4f7d9a152f8b48fded2bb03e89d8dbe
geara0 <adrezz.26.5@gmail.com>
2025-05-18 23:43:23 +0500

    fix isar for android

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,31 @@ allprojects {
     }
 }
 
+subprojects {
+    afterEvaluate {
+        // Для Application модулей
+        extensions.findByType<com.android.build.api.dsl.ApplicationExtension>()?.apply {
+            compileSdk = 34
+            buildToolsVersion = "34.0.0"
+
+            if (namespace == null) {
+                namespace = project.group.toString()
+            }
+        }
+
+        // Для Library модулей
+        extensions.findByType<com.android.build.api.dsl.LibraryExtension>()?.apply {
+            compileSdk = 34
+            buildToolsVersion = "34.0.0"
+
+            if (namespace == null) {
+                namespace = project.group.toString()
+            }
+        }
+    }
+}
+
+
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
